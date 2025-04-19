6cc160c4496625989e1f7238b11577ec07a6e358
Luca Clemente <clementeluca@proton.me>
2025-04-19 18:04:13 -0300

    Arquivos iniciais e primeiras impressões

---
+++ doc.md
@@ -0,0 +1,19 @@
+# Data Science for dummies
+
+## Pandas
+
+Pandas é uma biblioteca Python desenvolvida para análise e manipulação de dados. O nome "Pandas" deriva de "Panel Data", um termo da econometria para conjuntos de dados multidimensionais.
+
+### Função do pandas
+- Permite importar dados de diversos formatos (Excel,CSV, SQL, etc.)
+- Oferece estruturas para limpeza e transformação de dados.
+- Facilita a análise exploratória dos dados.
+- Prepara os dados para modelagem estatística e machine learning.
+
+### O que é um DataFrame?
+
+Um DataFrame é como uma tabela bidimensional no Excel, mas dessa vez, no Python.
+- Linhas
+- Colunas
+- Dados heterogêneos (diferentes tipos em diferentes colunas)
+![DataFrame no Python](/imgs/dataframe.svg)
---
+++ main.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+# dados = {
+#   'Nome': ['Ana','Bruno','Carlos','Diana'],
+#   'Idade': [25,30,22,28],
+#   'Cidade':['São Paulo','Rio De Janeiro','Belo horizonte','Porto Alegre']
+# }
+
+# df = pd.DataFrame(dados)
+
+# # Visualizar o DataFrame
+# print(df)
+
+# #Acessar uma coluna
+# print('Coluna de idades:')
+# print(df['Idade'])
+# print(df['Nome'])
+
+# # Informações básicas
+# print('Informações do DataFrame:')
+# print(df.info())
+
+# # Estatísticas descritivas:
+# print('Estatísticas descritivas:')
+# print(df.describe())
+
+dados2 = {
+  'Aluno': ['Ana','Bruno','Carlos','Diana','Eduardo'],
+  'Matemática':[85,78,92,88,76],
+  'Português':[90,82,78,95,86],
+  'Ciências':[88,81,85,92,79]
+}
+
+notas = pd.DataFrame(dados2)
+
+print(notas)
+
+# Acessando uma coluna específica:
+print('Notas de matemática:')
+print(notas['Matemática'])cle
+
+# Selecionando um subconjunto de dados:
+print('Alunos com nota de Matemática acima de 80')
+print(notas[notas['Matemática'] > 80])
+
+# Adicionando uma nova coluna (média das notas)
+notas['Média'] = (notas['Matemática'] + notas['Português'] + notas['Ciências']) / 3
+print('DataFrame com a coluna de média:')
+print(notas)
